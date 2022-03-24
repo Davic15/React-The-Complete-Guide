30fca943df2cb9c7c1322d8423a160e0f9e2b0b0
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-03-24 12:12:11 +0100

    Section 03: React Basics & Working with Components

--- section-03/src/components/ExpenseItem.js
+++ section-03/src/components/ExpenseItem.js
@@ -1,7 +1,20 @@
 //* Every new component starts with uppercase and camel case.
 //* A component in React.js is just a function of JavaScript
+//* You must have only one root tag for every return.
+//* It looks like HTML but it is not.
+//* Under the hood this is JavaScript.
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>;
+    return ( 
+        <div className='expense-item'>
+            <div>March 28th 2021</div>
+            <div className='expense-item__description'>
+                <h2>Car Insurance</h2>
+                <div className='expense-item__price'>$294.67</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
