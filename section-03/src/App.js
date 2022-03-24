81ec643cdbb78cb924d23a006f20c785839c8fcc
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-03-24 14:29:20 +0100

    Section 03: React Basics & Working with Components

--- section-03/src/App.js
+++ section-03/src/App.js
@@ -5,7 +5,7 @@
 //* With react we can generate out HTML without write too much code.
 //* New Component in new files
 //* The App component will render into single HTML page. the other components will come after App component
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -38,26 +38,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem 
-        title={expenses[0].title} 
-        amount={expenses[0].amount} 
-        date={expenses[0].date}
-      />
-      <ExpenseItem 
-        title={expenses[1].title} 
-        amount={expenses[1].amount} 
-        date={expenses[1].date}
-      />
-      <ExpenseItem 
-        title={expenses[2].title} 
-        amount={expenses[2].amount} 
-        date={expenses[2].date}
-      />
-      <ExpenseItem 
-        title={expenses[3].title} 
-        amount={expenses[3].amount} 
-        date={expenses[3].date}
-      />
+      <Expenses items={expenses}/>
     </div>
   );
 }
