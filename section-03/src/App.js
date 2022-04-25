26c4d1fbc62a0c96a144f3a40b9289e07ad79d02
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-04-25 21:49:30 +0200

    Section 5: Rendring Lists & Conditional Content

--- section-03/src/App.js
+++ section-03/src/App.js
@@ -5,40 +5,44 @@
 //* With react we can generate out HTML without write too much code.
 //* New Component in new files
 //* The App component will render into single HTML page. the other components will come after App component
+import React, { useState } from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
-const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Car insurance', 
-      amount: 294.67, 
-      date: new Date(2022, 0, 24)
-    },
-    {
-      id: 'e2',
-      title: 'Toilet Paper', 
-      amount: 94.12, 
-      date: new Date(2022, 2, 12)
-    },
-    {
-      id: 'e3',
-      title: 'New Tv', 
-      amount: 500.25, 
-      date: new Date(2022, 1, 14)
-    },
-    {
-      id: 'e4',
-      title: 'New Desk', 
-      amount: 150.55, 
-      date: new Date(2022, 0, 31)
-    }
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Car insurance', 
+    amount: 294.67, 
+    date: new Date(2022, 0, 24)
+  },
+  {
+    id: 'e2',
+    title: 'Toilet Paper', 
+    amount: 94.12, 
+    date: new Date(2022, 2, 12)
+  },
+  {
+    id: 'e3',
+    title: 'New Tv', 
+    amount: 500.25, 
+    date: new Date(2020, 1, 14)
+  },
+  {
+    id: 'e4',
+    title: 'New Desk', 
+    amount: 150.55, 
+    date: new Date(2021, 0, 31)
+  }
+];
 
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense)
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses ((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   }
 
   //* Send data (props), in the component we can access them with props.title, props.amount, props.date
