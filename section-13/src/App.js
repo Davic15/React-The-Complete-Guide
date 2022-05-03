6454354232f5e592c136b96dc797ead373bcca06
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-05-03 19:41:58 +0200

    Section 13: An Alternative way of Building Components: Class-based components

--- section-13/src/App.js
+++ section-13/src/App.js
@@ -1,11 +1,39 @@
-import Users from './components/Users';
+ import UserFinder from './components/UserFinder';
+ import UsersContext from './store/users-context';
 
-function App() {
-  return (
-    <div>
-      <Users />
-    </div>
-  );
-}
+/**
+ *  When we write components we use a function that receives props and return the jsx.
+ *  They are regular JS functions.
+ * 
+ *  Class-based components is an alternative to functions. Classes exist in modern JS.
+ *  Class Product extends Component{ render() { return () } }
+ *  Nowaday is common to use functions components instead of class-based components.
+ * 
+ *  Traditional react used class-based components, but they can't use react hooks.
+ * 
+ *  Component Life Cycle
+ *  componentDidMount()     Called once component mounted (was evaluated and rendered) equivalent to useEffect(.., []).
+ *  componentDidUpdate()    Called once component updated (was evaluated and rendered) equivalent to useEffect(.., [someValue]).
+ *  componentWillUnmount()  Called right before component is unmounted (removed from DOM) equivalent to useEffect(() => {return() =>{...}}, [])
+ */
 
-export default App;
+
+ const DUMMY_USERS = [
+   { id: 'u1', name: 'Max' },
+   { id: 'u2', name: 'Manuel' },
+   { id: 'u3', name: 'Julie' },
+ ];
+ 
+ function App() {
+   const usersContext = {
+     users: DUMMY_USERS
+   }
+ 
+   return (
+     <UsersContext.Provider value={usersContext}>
+       <UserFinder />
+     </UsersContext.Provider>
+   );
+ }
+ 
+ export default App;
---
+++ section-13/src/components/UserFinder.js
@@ -0,0 +1,48 @@
+import { Fragment, Component } from 'react';
+import UsersContext from '../store/users-context';
+import classes from './UserFinder.module.css'
+
+import Users from './Users';
+
+class UserFinder extends Component {
+    static contextType = UsersContext;
+    constructor() {
+        super();
+        this.state = {
+            filteredUsers: [],
+            searchTerm: ''
+        }
+    };
+
+    componentDidMount() {
+        //Send http request..
+        this.setState({ filteredUsers: this.context.users })
+    }
+
+    //* Method will be called when it will be re-evaluated.
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.searchTerm !== this.state.searchTerm) {
+            this.setState({ filteredUsers: 
+                this.context.users.filter((user) => user.name.includes(this.state.searchTerm))
+            });
+        }
+    }
+
+    searchChangeHandler(event) {
+        this.setState({searchTerm: event.target.value})
+    }
+
+    render() {
+        return(
+            <Fragment>
+                <div className={classes.finder}>
+                    <input type='search' onChange={this.searchChangeHandler.bind(this)} />
+                </div>
+                <Users users={this.state.filteredUsers} />
+            </Fragment>
+        )
+        
+    }
+}
+
+export default UserFinder;
